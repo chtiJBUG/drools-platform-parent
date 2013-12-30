2da9f3244aa82d13568b47422d3a40a777d9feb5
nheron <nicolas.heron.java@gmail.com>
2013-12-30 16:00:57 +0100

    Start OrientDB work

--- drools-platform-event-logger-server/src/main/java/org/chtijbug/drools/platform/logger/server/jms/JMSHistoryEventListener.java
+++ drools-platform-event-logger-server/src/main/java/org/chtijbug/drools/platform/logger/server/jms/JMSHistoryEventListener.java
@@ -1,11 +1,14 @@
 package org.chtijbug.drools.platform.logger.server.jms;
 
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentPool;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 
-import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
-import javax.jms.TextMessage;
+import javax.jms.ObjectMessage;
 
 
 /**
@@ -16,16 +19,42 @@ import javax.jms.TextMessage;
  */
 public class JMSHistoryEventListener implements MessageListener {
 
-  private static final Logger LOG = Logger.getLogger(JMSHistoryEventListener.class);
+    private static final Logger LOG = Logger.getLogger(JMSHistoryEventListener.class);
 
-  public void onMessage(Message message) {
-      try {
-       TextMessage msg = (TextMessage) message;
-       LOG.info("Consumed message: " + msg.getText());
-      } catch (JMSException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-      }
-  }
+
+    @Value("${orientdb.url}")
+    private String orientdb_url;
+    @Value("${guvnor.dbname}")
+    private String orientdb_dbName;
+
+    @Value("${orientdb.username}")
+    private String orientdb_username;
+    @Value("${orientdb.password}")
+    private String orientdb_password;
+
+    ODatabaseDocument database=null;
+
+    public void onMessage(Message message) {
+        try {
+            if (database== null){
+                database = ODatabaseDocumentPool.global().acquire(orientdb_url+orientdb_dbName,orientdb_username,orientdb_password);
+            }
+            ODocument toto = new ODocument("historyEvent");
+            toto.save();
+            //toto.
+            /**
+            OSQLSynchQuery<ODocument> query = new OSQLSynchQuery<ODocument>("");
+            List<ODocument> result = database.command(query).execute();
+            for (ODocument oDocument : result)  {
+                oDocument.field
+            }
+             **/
+            ObjectMessage msg = (ObjectMessage) message;
+            LOG.info("Consumed message: " + msg.toString());
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
