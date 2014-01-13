af34a1d0db08af47bc62501613d80bea3607171b
nheron <nicolas.heron.java@gmail.com>
2014-01-13 10:17:32 +0100

    Start websocket server for runtime

---
+++ drools-platform-core/src/main/java/org/chtijbug/drools/platform/core/Main.java
@@ -0,0 +1,13 @@
+package org.chtijbug.drools.platform.core;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 10/01/14
+ * Time: 18:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main (String[] args){
+       System.out.println("Hello World");
+      }
+}
