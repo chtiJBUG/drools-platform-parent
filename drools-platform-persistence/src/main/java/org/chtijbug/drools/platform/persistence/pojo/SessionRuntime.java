753ebc90a75dda2a7765badd26d1d4fdec97b9ee
nheron <nicolas.heron.java@gmail.com>
2014-02-17 16:41:51 +0100

    more Event and JUNIT tests

---
+++ drools-platform-persistence/src/main/java/org/chtijbug/drools/platform/persistence/pojo/SessionRuntime.java
@@ -0,0 +1,10 @@
+package org.chtijbug.drools.platform.persistence.pojo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 14/02/14
+ * Time: 09:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class SessionRuntime {
+}
