c0b9a2a2aacf67d465d030077825c8cb6a9d81ad
nheron <nicolas.heron.java@gmail.com>
2014-06-02 15:10:57 +0200

    Add PlatformRuntime Controller

---
+++ drools-platform-manager/drools-platform-manager-ui/src/main/java/org/chtijbug/drools/platform/web/RuntimeResource.java
@@ -0,0 +1,48 @@
+package org.chtijbug.drools.platform.web;
+
+import org.chtijbug.drools.platform.persistence.PlatformRuntimeInstanceRepository;
+import org.chtijbug.drools.platform.persistence.pojo.PlatformRuntimeInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 02/06/14
+ * Time: 13:53
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+@RequestMapping(value = "/runtime_resource")
+public class RuntimeResource {
+    @Autowired
+    PlatformRuntimeInstanceRepository platformRuntimeInstanceRepository;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/activeSessions/{packageName:.+}")
+    @Consumes(value = MediaType.APPLICATION_JSON)
+    @Produces(value = MediaType.APPLICATION_JSON)
+    @ResponseBody
+    public List<PlatformRuntimeInstance> findActivePlatformRuntimeInstance(@PathVariable String packageName) {
+        return platformRuntimeInstanceRepository.findByPackageNameActiveRuntime(packageName);
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/allSessions/{packageName:.+}")
+    @Consumes(value = MediaType.APPLICATION_JSON)
+    @Produces(value = MediaType.APPLICATION_JSON)
+    @ResponseBody
+    public List<PlatformRuntimeInstance> findAllPlatformRuntimeInstance(@PathVariable String packageName) {
+        return platformRuntimeInstanceRepository.findByPackageNameAllRuntime(packageName);
+
+    }
+
+}
--- drools-platform-manager/drools-platform-persistence/src/main/java/org/chtijbug/drools/platform/persistence/PlatformRuntimeInstanceRepository.java
+++ drools-platform-manager/drools-platform-persistence/src/main/java/org/chtijbug/drools/platform/persistence/PlatformRuntimeInstanceRepository.java
@@ -3,6 +3,8 @@ package org.chtijbug.drools.platform.persistence;
 
 import org.chtijbug.drools.platform.persistence.pojo.PlatformRuntimeInstance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -16,11 +18,29 @@ public interface PlatformRuntimeInstanceRepository extends JpaRepository<Platfor
 
     List<PlatformRuntimeInstance> findByRuleBaseIDAndEndDateNull(Integer ruleBaseId);
 
-    List<PlatformRuntimeInstance>  findByRuleBaseIDAndShutdowDateNull(Integer ruleBaseId);
+    List<PlatformRuntimeInstance> findByRuleBaseIDAndShutdowDateNull(Integer ruleBaseId);
 
     PlatformRuntimeInstance findByRuleBaseID(Integer ruleBaseId);
 
     PlatformRuntimeInstance findByRuleBaseIDAndStartDateAndEndDateNull(Integer ruleBaseId, Date startDate);
 
     public List<PlatformRuntimeInstance> findByHostnameAndEndDateNull(String hostname);
+
+    @Query(value = "select pri " +
+            "from PlatformRuntimeInstance pri,PlatformRuntimeDefinition prd ,DroolsResource drs " +
+            "where pri.platformRuntimeDefinition = prd " +
+            "and drs member of prd.droolsRessourcesDefinition " +
+            "and pri.endDate is null " +
+            "and drs.guvnor_packageName=:packageName")
+    public List<PlatformRuntimeInstance> findByPackageNameActiveRuntime(@Param("packageName") String packageName);
+
+
+    @Query(value = "select pri " +
+            "from PlatformRuntimeInstance pri,PlatformRuntimeDefinition prd ,DroolsResource drs " +
+            "where pri.platformRuntimeDefinition = prd " +
+            "and drs member of prd.droolsRessourcesDefinition " +
+            "and drs.guvnor_packageName=:packageName")
+    public List<PlatformRuntimeInstance> findByPackageNameAllRuntime(@Param("packageName") String packageName);
+
+
 }
