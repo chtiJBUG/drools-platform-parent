253d73d4449f82fa8ea3c66b91339220214bc0cc
nheron <nicolas.heron.java@gmail.com>
2014-07-30 14:07:04 +0200

    Add Custom repository for SessionExecution class

--- drools-platform-manager/drools-platform-persistence/src/main/java/org/chtijbug/drools/platform/persistence/SessionExecutionRepository.java
+++ drools-platform-manager/drools-platform-persistence/src/main/java/org/chtijbug/drools/platform/persistence/SessionExecutionRepository.java
@@ -3,23 +3,16 @@ package org.chtijbug.drools.platform.persistence;
 
 import org.chtijbug.drools.platform.persistence.pojo.SessionExecution;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 /**
  * Cf. http://docs.spring.io/spring-data/jpa/docs/1.4.2.RELEASE/reference/html/jpa.repositories.html
  */
 @Component
-public interface SessionExecutionRepository extends JpaRepository<SessionExecution, Long> {
+public interface SessionExecutionRepository extends JpaRepository<SessionExecution, Long>, SessionExecutionCustomRepository {
 
 
-    @Query("select s from SessionExecution s " +
-             "where s.platformRuntimeInstance.ruleBaseID= :ruleBaseID and s.sessionId = :sessionId  "+
-             "and s.platformRuntimeInstance.endDate is null  ")
-    SessionExecution findByRuleBaseIDAndSessionIdAndEndDateIsNull(@Param("ruleBaseID") Integer ruleBaseID, @Param("sessionId") Integer sessionId);
 
 
     public SessionExecution findDetailsBySessionId(@Param("sessionId") Integer sessionId);
